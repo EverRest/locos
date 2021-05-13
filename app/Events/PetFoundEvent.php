7af0a794f25d5299ce0aef9092724279df30ff12
pavlo <medynskyypavlo@gmail.com>
2021-05-13 17:54:15 +0300

    integrate redis + socket.io + event

---
+++ app/Events/PetFoundEvent.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Accident;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Class PetFoundEvent
+ *
+ * @package App\Events
+ */
+class PetFoundEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var Accident
+     */
+    private $accident;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Accident $accident)
+    {
+        $this->accident = $accident;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('found');
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @author Author
+     *
+     * @return array
+     */
+    public function broadcastWith(): array
+    {
+        return [
+            'id' => $this->accident->id,
+            'accident' => $this->accident,
+
+        ];
+    }
+}
